7a7d893ae1dc5dcf2a42a5d3b060ab5a1aa44a0a
testrepo-git <mary.jose@simelabs.com>
2021-10-23 23:25:54 +0530

    multiples pgm added

--- Main.java
+++ Main.java
@@ -59,4 +59,40 @@ public class Prime {
 
 	}
 }
+public class Multiple{
+    private int i = 0;
+    private int n = 0;
 
+    public static void m() {
+
+        int i = 0;
+        int n = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        i = input.nextInt();
+        System.out.print("Enter no of multiples: ");
+        n = input.nextInt();
+
+        if ((i <= 1) || (n <= 1)) {
+            System.out.println("Please enter numbers above 1");
+            System.exit(1);
+        } else {
+            System.out.print(n + " multiples of " + i + " are: ");
+            mult(i, n);
+        }
+
+    }
+
+    public static void mult(int i, int n) {
+        int[] arr = new int[n];
+        int count = 2;
+        for (int x = 0; x < n; x++) {
+            arr[x] = i * count++;
+        }
+        for (int y = 0; y < arr.length; y++) {
+            System.out.print(arr[y] + " ");
+        }
+    }
+}
